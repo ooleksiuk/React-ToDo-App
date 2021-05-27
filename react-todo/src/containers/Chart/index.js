b43d44e1a6521144c7a6c84dc8fb56d01fe33d5e
Oleksiuk <ooleksiuk.exceedteam@gmail.com>
2021-05-27 13:34:03 +0300

    Basic React Router navigation is added

---
+++ react-todo/src/containers/Chart/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+
+const Chart = () => {
+  return <h1> Chart!</h1>;
+};
+
+export default Chart;
--- react-todo/src/index.js
+++ react-todo/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import List from './containers/List';
+import Chart from './containers/Chart';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './store';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 const store = createStore(
   reducers,
@@ -15,7 +17,23 @@ const store = createStore(
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <List />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Todo</Link>
+            </li>
+            <li>
+              <Link to="/chart">Chart</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={List} />
+          <Route path="/chart" component={Chart} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
